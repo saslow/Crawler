2cfe4366d3873a2d89ff9e9089df5f7869faceaf
BobKet18rus <pasha.rys4567@gmail.com>
2024-09-13 19:34:40 +0300

    redoning

--- addons/LinkedCamera2D/linked_camera2D.gd
+++ addons/LinkedCamera2D/linked_camera2D.gd
@@ -9,46 +9,50 @@ func _ready():
 		#modulate = Color(0, 0 , 0, 0.5)
 	#else:
 		#clone_to_bg.call_deferred("add_child", self)
-	
-	ch.limit_changed.connect(_on_limits_changed)
-	ch.room_changed.connect(_on_room_changed)
-	ch.force_cameras.connect(_on_force_cameras)
-	if get_parent() is Layer2D:
-		zoom = zoom / get_parent().z
-	#limit_left *= get_parent().z
-	#limit_right *= get_parent().z
-	if get_parent().z == 1:
-		limit_left += 960
-		limit_right -= 960
+	if !Engine.is_editor_hint():
+		ch.limit_changed.connect(_on_limits_changed)
+		ch.room_changed.connect(_on_room_changed)
+		ch.force_cameras.connect(_on_force_cameras)
+		if get_parent() is Layer2D:
+			zoom = zoom / get_parent().z
+		#limit_left *= get_parent().z
+		#limit_right *= get_parent().z
+		if get_parent().z == 1:
+			limit_left += 960
+			limit_right -= 960
 
 	limit_top *= get_parent().z
 	limit_bottom *= get_parent().z
 		
 func _process(delta):
-	global_position.x = ch.global_position.x
-	global_position.y = ch.global_position.y * (get_parent().z / ch.current_player_level )
+	if !Engine.is_editor_hint():
+		global_position.x = ch.global_position.x
+		global_position.y = ch.global_position.y * (get_parent().z / ch.current_player_level )
 
 func _on_force_cameras() -> void:
-	position_smoothing_enabled = false
-	position_smoothing_enabled = true
-	reset_smoothing()
+	if !Engine.is_editor_hint():
+		position_smoothing_enabled = false
+		position_smoothing_enabled = true
+		reset_smoothing()
 
 func _on_room_changed(offset_x : float, offset_y : float) -> void:
-	limit_bottom += offset_y
-	limit_left += offset_x
-	limit_right += offset_x
-	limit_top += offset_y
+	if !Engine.is_editor_hint():
+		limit_bottom += offset_y
+		limit_left += offset_x
+		limit_right += offset_x
+		limit_top += offset_y
 
 func _on_limits_changed(left : float, top : float, right : float, bottom : float) -> void:
-	limit_left = left
-	limit_right = right
-	limit_top = top
-	limit_bottom = bottom
-	print("limits changed")
-	#limit_left *= get_parent().z
-	#limit_right *= get_parent().z
-	if get_parent().z == 1:
-		limit_left += 960
-		limit_right -= 960
-	limit_top *= get_parent().z
-	limit_bottom *= get_parent().z
+	if !Engine.is_editor_hint():
+		limit_left = left
+		limit_right = right
+		limit_top = top
+		limit_bottom = bottom
+		print("limits changed")
+		#limit_left *= get_parent().z
+		#limit_right *= get_parent().z
+		if get_parent().z == 1:
+			limit_left += 960
+			limit_right -= 960
+		limit_top *= get_parent().z
+		limit_bottom *= get_parent().z
--- scripts/level_parts/rooms/room_changer.gd
+++ scripts/level_parts/rooms/room_changer.gd
@@ -12,27 +12,26 @@ func _on_body_exited(body):
 	origin_shifting(body)
 	
 func transition_forward() -> void:
-	#g.current_level.UI.anim.play("fade")
-	print("forward")
+	g.current_level.UI.anim.play("fade")
+	print_debug("forward")
 	ch.force_cameras.emit()
-	
+	await get_tree().create_timer(0.15).timeout
 	get_layer_from_level(0).global_position.x -= layers_offset_x
 	get_layer_from_level(1).global_position.x -= layers_offset_x
-	#g.player.global_position.x = global_position.x + PLAYER_OFFSET
-	#g.player.global_position.y = global_position.y
+	g.player.global_position.x = global_position.x + PLAYER_OFFSET
+	g.player.global_position.y = global_position.y
 	ch.room_changed.emit(-layers_offset_x, 0)
 	ch.force_cameras.emit()
 
 func transition_backward() -> void:
-	#g.current_level.UI.anim.play("fade")
-	print("backward")
+	g.current_level.UI.anim.play("fade")
+	print_debug("backward")
 	ch.force_cameras.emit()
-	#
-
+	await get_tree().create_timer(0.15).timeout
 	get_layer_from_level(0).global_position.x += layers_offset_x
 	get_layer_from_level(1).global_position.x += layers_offset_x
-	#g.player.global_position.x = global_position.x - PLAYER_OFFSET
-	#g.player.global_position.y = global_position.y
+	g.player.global_position.x = global_position.x - PLAYER_OFFSET
+	g.player.global_position.y = global_position.y
 	ch.room_changed.emit(layers_offset_x, 0)
 	ch.force_cameras.emit()
 
